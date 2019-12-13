570b8563e22afcc8fb7148b7bf5eabd0469a32a6
Marley Kanui <marley.kanui@gmail.com>
2019-12-13 17:11:03 +0000

    Linked List 1.0

---
+++ linked_list_pseudo_code.rb
@@ -0,0 +1,17 @@
+# Kanui Komments
+# After reading through the linked list overview documentation, I know that a
+# linked lists consist of a set of 'nodes' or pieces of data the conatain both
+# data and a reference to the next node in line. 
+
+# Kanui Pseudo Kode
+
+# Consider the index of each node, then 
+  # Alter the index of each node, to be in reverse order, then 
+    # Output the nodes in the new reversed order 
+
+# Kanui Psuedo Kode - Abstraction 1
+
+# Create a loop that will iterate over each of the nodes, then 
+  # Create a conditional statement stating that if the value = nil place at the end of the list, then 
+    # Create a method to re-order the node given there reference position 
+      
